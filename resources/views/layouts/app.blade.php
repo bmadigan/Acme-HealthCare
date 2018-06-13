e3ea398f33ab47c02035268d089b3c420e9313eb
Brad Madigan <bradmadigan@gmail.com>
2018-06-13 11:44:37 -0400

    Added App layout and general styles for Nav and Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,31 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-  </head>
-  <body class="font-sans antialiased text-black leading-tight">
-    <div id="app">
-      @yield('body')
-    </div>
-    <script src="{{ mix('js/app.js') }}"></script>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    </head>
+    <body class="font-sans antialiased bg-beige-light text-black">
+
+        <div id="app">
+            <div class="bg-blue-darker mb-6">
+                @include('layouts.partials.topnav')
+            </div>
+            <div class="container mx-auto">
+                @yield('body')
+            </div>
+            <div class="bg-white border-t-2 border-beige fixed pin-b pin-x">
+                @include('layouts.partials.footer')
+            </div>
+        </div>
+
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
 </html>
